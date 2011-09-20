198c8b2b93db5d86234f196028e15652b1a445fe
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2011-09-20 14:09:44 -0500

    supporting multiple values to a single filter

--- lib/tablificate/base.rb
+++ lib/tablificate/base.rb
@@ -1,26 +1,32 @@
 module Tablificate
   class Base
     def self.find_by_params(params)
-      v = @scope
+      scope = @scope
 
       # filtering
       if params[:filter]
         params[:filter].each do |name, value|
           next if value.blank?
 
-          value.strip!
+          name = name.to_sym
+          value.strip! if value.is_a?(String)
 
-          if @filter && @filter[name.to_sym]
-            name = name.to_sym
-            if @filter[name].class == Proc
-              v = @filter[name].call(v, value)
-            else
+          if @filter and @filter[name]
+            if @filter[name].is_a?(Proc)
+              scope = @filter[name].call(scope, value)
+            elsif value.is_a?(String)
               value = "%#{value}%" if @filter[name][:match] == 'contains'
 
-              v = v.where(["#{@filter[name][:field]} LIKE ?", value])
+              scope = scope.where(["#{@filter[name][:field]} LIKE ?", value])
+            elsif value.is_a?(Array)
+              if @filter[name][:match] == 'contains'
+                scope = scope.where(["#{name.to_s.gsub(/\W/, '')} REGEXP ?", value.join('|')])
+              else
+                scope = scope.where(name => value)
+              end
             end
           else
-            v = v.where(["#{name.gsub(/\W/, '')} LIKE ?", value])
+            scope = scope.where(name => value)
           end
         end
       end
@@ -29,21 +35,21 @@ module Tablificate
       column = params[:sort] || (@default_sort && @default_sort[0])
       dir    = params[:dir]  || (@default_sort && @default_sort[1])
       if column.present?
-        v = v.order((@sort && @sort[column.to_sym]) || "#{column.to_s} ASC")
+        scope = scope.order((@sort && @sort[column.to_sym]) || "#{column.to_s} ASC")
         if dir == 'desc'
-          v = v.reverse_order
+          scope = scope.reverse_order
         end
       end
 
       # return an arel object with our data attached
-      v = v.tablificate_ext
+      scope = scope.tablificate_ext
       sorting = {column: nil, dir: nil}
       if column.present?
         sorting[:column] = column.to_sym
         sorting[:dir]    = (dir.present? and ['asc', 'desc'].include?(dir)) ? dir : 'asc'
       end
-      v.tablificate_add_data(:current_sort, sorting)
-      v
+      scope.tablificate_add_data(:current_sort, sorting)
+      scope
     end
 
     def self.scope(model = nil, &block)
