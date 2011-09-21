737d82dc659f5d6c94757851a52b20f319aef0db
Aaron Lasseigne <alasseigne@sei-mi.com>
2011-09-21 16:08:47 -0500

    better table scoping for columns including no automatically scoping them to the base table

--- lib/tablificate/base.rb
+++ lib/tablificate/base.rb
@@ -17,20 +17,24 @@ module Tablificate
             elsif value.is_a?(String)
               value = "%#{value}%" if @filter[name][:match] == 'contains'
 
-              scope = scope.where(["#{@filter[name][:field]} LIKE ?", value])
+              scope = scope.where(["#{get_full_column_name(@filter[name][:field])} LIKE ?", value])
             elsif value.is_a?(Array)
+              full_column_name = get_full_column_name(@filter[name][:field])
+
               if @filter[name][:match] == 'contains'
-                scope = scope.where(["#{name.to_s.gsub(/\W/, '')} REGEXP ?", value.join('|')])
+                scope = scope.where(["#{full_column_name} REGEXP ?", value.join('|')])
               else
-                scope = scope.where(@filter[name][:field] => value)
+                scope = scope.where(["#{full_column_name} IN(?)", value])
               end
             elsif value.is_a?(Hash)
-              scope = scope.where(["#{@filter[name][:field].to_s.gsub(/\W/, '')} BETWEEN :start AND :stop", value])
+              scope = scope.where(["#{get_full_column_name(@filter[name][:field])} BETWEEN :start AND :stop", value])
             end
+          elsif value.is_a?(Array)
+            scope = scope.where(["#{get_full_column_name(name.to_s.gsub(/\W/, ''))} IN(?)", value])
           elsif value.is_a?(Hash)
-            scope = scope.where(["#{name.to_s.gsub(/\W/, '')} BETWEEN :start AND :stop", value])
+            scope = scope.where(["#{get_full_column_name(name.to_s.gsub(/\W/, ''))} BETWEEN :start AND :stop", value])
           else
-            scope = scope.where(name => value)
+            scope = scope.where(["#{get_full_column_name(name.to_s.gsub(/\W/, ''))} LIKE ?", value])
           end
         end
       end
@@ -39,7 +43,7 @@ module Tablificate
       column = params[:sort].try(:gsub, /\W/, '') || @default_sort.try(:[], 0)
       dir    = params[:dir]                       || @default_sort.try(:[], 1)
       if column.present?
-        scope = scope.order(@sort.try(:[], column.to_sym) || "#{@scope.column_names.include?(column.to_s) ? @scope.table_name + '.' : ''}#{column.to_s} ASC")
+        scope = scope.order(@sort.try(:[], column.to_sym) || "#{get_full_column_name(column.to_s)} ASC")
         if dir == 'desc'
           scope = scope.reverse_order
         end
@@ -87,5 +91,16 @@ module Tablificate
         @filter[name] = options
       end
     end
+
+    def self.get_full_column_name(name)
+      name = name.to_s
+
+      if @scope.column_names.include?(name)
+        "#{@scope.table_name}.#{name}"
+      else
+        name
+      end
+    end
+    private_class_method :get_full_column_name
   end
 end
