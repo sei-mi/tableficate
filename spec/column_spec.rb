ddaeeeeda20274b160f4355b9ab36c6be62a8777
Aaron Lasseigne <alasseigne@sei-mi.com>
2011-09-26 16:21:43 -0500

    fixing column spec test

--- spec/column_spec.rb
+++ spec/column_spec.rb
@@ -3,12 +3,12 @@ require 'tablificate/column'
 
 describe Tablificate::Column do
   it 'should generate a header if none is provided' do
-    column = Tablificate::Column.new(nil, nil, :alter_ego)
+    column = Tablificate::Column.new(nil, :alter_ego)
 
     column.header.should == 'Alter Ego'
   end
   it 'should show the header if provided' do
-    column = Tablificate::Column.new(nil, nil, :superhero_union_id, header: 'SUID')
+    column = Tablificate::Column.new(nil, :superhero_union_id, header: 'SUID')
 
     column.header.should == 'SUID'
   end
@@ -17,7 +17,7 @@ describe Tablificate::Column do
     row = mock('SuperHero')
     row.stub!(:alter_ego).and_return('Walter Kovacs')
 
-    column = Tablificate::Column.new(nil, nil, :alter_ego)
+    column = Tablificate::Column.new(nil, :alter_ego)
 
     column.value(row).should == 'Walter Kovacs'
   end
@@ -25,19 +25,19 @@ describe Tablificate::Column do
     row = mock('SuperHero')
     row.stub!(:birthdate).and_return(Time.utc(2000, 9, 12, 13, 45, 37))
 
-    column = Tablificate::Column.new(nil, nil, :born_at, format: Proc.new {|row| row.birthdate.strftime('%B %d, %Y at %I:%M:%S %P')})
+    column = Tablificate::Column.new(nil, :born_at, format: Proc.new {|row| row.birthdate.strftime('%B %d, %Y at %I:%M:%S %P')})
 
     column.value(row).should == 'September 12, 2000 at 01:45:37 pm'
   end
 
   it 'should allow sorting to be turned on and off' do
-    column = Tablificate::Column.new(nil, nil, :foo, show_sort: false)
+    column = Tablificate::Column.new(nil, :foo, show_sort: false)
     column.show_sort?.should be false
 
-    column = Tablificate::Column.new(nil, nil, :foo, show_sort: true)
+    column = Tablificate::Column.new(nil, :foo, show_sort: true)
     column.show_sort?.should be true
 
-    column = Tablificate::Column.new(nil, nil, :foo)
+    column = Tablificate::Column.new(nil, :foo)
     column.show_sort?.should be false
   end
 end
