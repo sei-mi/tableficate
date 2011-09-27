1af9ed09cccba9f67917ed20a3fb47088d522efe
Aaron Lasseigne <alasseigne@sei-mi.com>
2011-09-27 11:35:20 -0500

    fixing up the column spec

--- spec/column_spec.rb
+++ spec/column_spec.rb
@@ -1,45 +1,61 @@
 require 'rails/all'
-require 'tablificate/column'
+require 'tablificate'
 
 describe Tablificate::Column do
   it 'should generate a header if none is provided' do
-    column = Tablificate::Column.new(nil, :alter_ego)
+    column = Tablificate::Column.new(nil, :first_name)
 
-    column.header.should == 'Alter Ego'
+    column.header.should == 'First Name'
   end
   it 'should show the header if provided' do
-    column = Tablificate::Column.new(nil, :superhero_union_id, header: 'SUID')
+    column = Tablificate::Column.new(nil, :school, header: 'Alma Mater')
 
-    column.header.should == 'SUID'
+    column.header.should == 'Alma Mater'
   end
 
   it 'should show the value from the database field if no alternative is provided' do
-    row = mock('SuperHero')
-    row.stub!(:alter_ego).and_return('Walter Kovacs')
+    row = mock('Agronomist')
+    row.stub!(:name).and_return('Norman Borlaug')
 
-    column = Tablificate::Column.new(nil, :alter_ego)
+    column = Tablificate::Column.new(nil, :name)
 
-    column.value(row).should == 'Walter Kovacs'
+    column.value(row).should == 'Norman Borlaug'
   end
   it 'should return the value provided from the block' do
-    row = mock('SuperHero')
-    row.stub!(:birthdate).and_return(Time.utc(2000, 9, 12, 13, 45, 37))
+    row = mock('Agronomist')
+    row.stub!(:birthdate).and_return(Time.utc(1914, 3, 25, 13, 45, 37))
 
     column = Tablificate::Column.new(nil, :born_at) do |row|
       row.birthdate.strftime('%B %d, %Y at %I:%M:%S %P')
     end
 
-    column.value(row).should == 'September 12, 2000 at 01:45:37 pm'
+    column.value(row).should == 'March 25, 1914 at 01:45:37 pm'
   end
 
   it 'should allow sorting to be turned on and off' do
-    column = Tablificate::Column.new(nil, :foo, show_sort: false)
+    column = Tablificate::Column.new(nil, :first_name, show_sort: false)
     column.show_sort?.should be false
 
-    column = Tablificate::Column.new(nil, :foo, show_sort: true)
+    column = Tablificate::Column.new(nil, :first_name, show_sort: true)
     column.show_sort?.should be true
 
-    column = Tablificate::Column.new(nil, :foo)
+    column = Tablificate::Column.new(nil, :first_name)
     column.show_sort?.should be false
+  end
+
+  it 'should indicate whether a column is sorted or not' do
+    table = Tablificate::Table.new(nil, [], {}, {current_sort: {column: :first_name, dir: 'asc'}})
+
+    column = Tablificate::Column.new(table, :first_name)
+    column.is_sorted?('asc').should be true
+
+    column = Tablificate::Column.new(table, :first_name)
+    column.is_sorted?('desc').should be false
+
+    column = Tablificate::Column.new(table, :first_name)
+    column.is_sorted?.should be true
+
+    column = Tablificate::Column.new(table, :last_name)
+    column.is_sorted?.should be false
   end
 end
