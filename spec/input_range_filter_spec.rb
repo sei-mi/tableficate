c5a06ba1ee885df67f247aa926aff1567d83b1f2
Aaron Lasseigne <alasseigne@sei-mi.com>
2011-09-28 00:48:52 -0500

    fixing InputRangeFilter spec

--- spec/input_range_filter_spec.rb
+++ spec/input_range_filter_spec.rb
@@ -2,15 +2,18 @@ require 'rails/all'
 require 'tablificate'
 
 describe Tablificate::InputRangeFilter do
-  it 'should find the correct template type' do
-    table = Tablificate::Table.new(nil, nil, {}, {})
-    table.column(:birth_date)
+  before(:each) do
+    @table = Tablificate::Table.new(nil, nil, {}, {})
+  end
 
-    Tablificate::InputRangeFilter.new(table, :birth_date).template.should == 'input_range_filter'
+  it 'should find the correct template type' do
+    @table.column(:birth_date)
+
+    Tablificate::InputRangeFilter.new(@table, :birth_date).template.should == 'input_range_filter'
   end
 
   it 'should create a start input and a stop input' do
-    filter = Tablificate::InputRangeFilter.new(nil, :birth_date, label: 'Birthday')
+    filter = Tablificate::InputRangeFilter.new(@table, :birth_date, label: 'Birthday')
 
     filter.start.name.should == 'birth_date_start'
     filter.stop.name.should == 'birth_date_stop'
@@ -20,11 +23,11 @@ describe Tablificate::InputRangeFilter do
   end
 
   it 'should use the label from the range call on the start and stop inputs if they do not already have a label' do
-    filter = Tablificate::InputRangeFilter.new(nil, :birth_date, label: 'Birthday')
+    filter = Tablificate::InputRangeFilter.new(@table, :birth_date, label: 'Birthday')
     filter.start.label.should == 'Birthday'
     filter.stop.label.should == 'Birthday'
 
-    filter = Tablificate::InputRangeFilter.new(nil, :birth_date, label: 'Birthday', start: {label: 'Start'}, stop: {label: 'Stop'})
+    filter = Tablificate::InputRangeFilter.new(@table, :birth_date, label: 'Birthday', start: {label: 'Start'}, stop: {label: 'Stop'})
     filter.start.label.should == 'Start'
     filter.stop.label.should == 'Stop'
   end
