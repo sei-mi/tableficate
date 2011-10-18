6889738449f517ddc2dff8f90a870861e3e36b6b
Aaron Lasseigne <alasseigne@sei-mi.com>
2011-10-18 12:30:25 -0500

    template_path now uses File.join

--- lib/tableficate/utils.rb
+++ lib/tableficate/utils.rb
@@ -1,7 +1,7 @@
 module Tableficate
   module Utils
     def self.template_path(template, theme = '')
-      ['tableficate', theme, template].delete_if(&:blank?).join('/')
+      File.join(['tableficate', theme, template].delete_if(&:blank?))
     end
   end
 end
