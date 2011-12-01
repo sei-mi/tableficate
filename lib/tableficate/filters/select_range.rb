2ec190f155336809e8d23c3ec489970ad5f0506d
Aaron Lasseigne <alasseigne@sei-mi.com>
2011-12-01 12:35:41 -0600

    fixing select range filters

--- lib/tableficate/filters/select_range.rb
+++ lib/tableficate/filters/select_range.rb
@@ -1,6 +1,54 @@
 module Tableficate
   module Filter
+    class SelectStart < Select
+      def initialize(table, name, options = {})
+        super(table, name, options)
+
+        @field_name += '[start]'
+      end
+
+      def name
+        "#{@name}_start".to_sym
+      end
+
+      def field_value(params)
+        params[:filter][@name][:start] rescue ''
+      end
+    end
+
+    class SelectStop < Select
+      def initialize(table, name, options = {})
+        super(table, name, options)
+
+        @field_name += '[stop]'
+      end
+
+      def name
+        "#{@name}_stop".to_sym
+      end
+
+      def field_value(params)
+        params[:filter][@name][:stop] rescue ''
+      end
+    end
+
     class SelectRange < Base
+      attr_reader :start, :stop
+
+      def initialize(table, name, options = {})
+        start_options = options.delete(:start) || {}
+        stop_options  = options.delete(:stop)  || {}
+
+        super(table, name, options)
+
+        start_options.reverse_merge!(@options)
+        start_options.reverse_merge!(label: self.label)
+        stop_options.reverse_merge!(@options)
+        stop_options.reverse_merge!(label: self.label)
+
+        @start = SelectStart.new(table, name, start_options)
+        @stop  = SelectStop.new(table, name, stop_options)
+      end
     end
   end
 end
