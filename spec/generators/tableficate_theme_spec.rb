215989f80e080df1686aad62316e0ddba2951a5a
Aaron Lasseigne <alasseigne@sei-mi.com>
2011-12-23 13:21:48 -0600

    adding additional tests

--- spec/generators/tableficate_theme_spec.rb
+++ spec/generators/tableficate_theme_spec.rb
@@ -8,4 +8,18 @@ describe 'tableficate:theme' do
       subject.should generate('app/views/tableficate/foo/_table.html.erb')
     end
   end
+
+  with_args :foo, 'table_for' do
+    it 'should generate a single file in app/views/tableficate/foo/' do
+      subject.should generate('app/views/tableficate/foo')
+      subject.should generate('app/views/tableficate/foo/_table_for.html.erb')
+    end
+  end
+
+  with_args :foo, 'filters/form' do
+    it 'should generate a single file in app/views/tableficate/foo/filters' do
+      subject.should generate('app/views/tableficate/foo/filters')
+      subject.should generate('app/views/tableficate/foo/filters/_form.html.erb')
+    end
+  end
 end
